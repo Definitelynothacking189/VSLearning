52f270c3493961ccf7f008f5c34c9a223bc68f61
Definitelynothacking189 <kambikanu@gmail.com>
2024-05-30 20:19:26 -0400

    first commit

---
+++ javascriptBasics/jsrtut.js
@@ -0,0 +1,6 @@
+let username;
+
+document.getElementById("mysubmit").onclick = function () {
+  username = document.getElementById("myText").value;
+  document;
+};
---
+++ javascriptBasics/rbasics.js
@@ -0,0 +1,11 @@
+//two ways to write functions
+
+//traditional requires export default
+function DoSomething() {
+
+}
+
+// new way good for callbacks and requires export
+const DoSomething = () => {
+
+}
